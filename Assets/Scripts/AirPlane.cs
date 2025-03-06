2eacc245b0f382975a3c726519cc1e1384e775de
Sarawut210747 <sarawut.wongsr@bumail.net>
2025-03-06 16:29:02 +0700

    End week7

--- Assets/Scripts/AirPlane.cs
+++ Assets/Scripts/AirPlane.cs
@@ -7,6 +7,9 @@ public class AirPlane : MonoBehaviour
     [SerializeField] float liftBosster;
     [SerializeField] float drag;
     [SerializeField] float anglarDrag;
+    [SerializeField] float yawPower;
+    [SerializeField] float pitchPower;
+    [SerializeField] float rollPower;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,6 +26,16 @@ public class AirPlane : MonoBehaviour
         }
         Vector3 lift = Vector3.Project(rb.linearVelocity, transform.forward);
         rb.AddForce(transform.up * lift.magnitude * liftBosster);
+        rb.linearDamping = rb.linearVelocity.magnitude * drag;
+        rb.angularDamping = rb.linearVelocity.magnitude * anglarDrag;
+
+        float yaw = Input.GetAxis("Horizontal") * yawPower;
+        float pitch = Input.GetAxis("Vertical") * pitchPower;
+        float roll = Input.GetAxis("Roll") * rollPower;
+
+        rb.AddTorque(transform.up * yaw);
+        rb.AddTorque(transform.right * pitch);
+        rb.AddTorque(transform.forward * roll);
     }
 
 }
